29cb6760fe516a2851b85f084d2ea9e06d6bfbca
Timothy Hackbarth <timhackbarth@Tim-Hackbarth.local>
2011-07-15 19:00:33 -0500

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qas::Application.config.secret_token = 'b58ca26c92cfa3f45c58b4d6aac5ef228ae5703352a204db850b4e824f4212f0d6140314f9decf400888fc000329f123c0fd059a90984d51d26920c35c1f3e17'
